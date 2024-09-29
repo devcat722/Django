63a95309e452cf6e3df591a4c83e14b8ef2394ba
devcat722 <pashannacaptain@gmail.com>
2024-09-29 14:22:41 +0530

    initial setup
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center h-screen font-extrabold">
+      App
+    </div>
+  )
+}
+
+export default App
