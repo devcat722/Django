8c27dc1eb0ab8479256b50ea7ebae94321cda6c9
devcat722 <pashannacaptain@gmail.com>
2024-09-29 20:56:53 +0530

    simple api setup
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

--- DjangoServer/api/views.py
+++ DjangoServer/api/views.py
@@ -4,3 +4,9 @@ from rest_framework.decorators import api_view
 @api_view(["GET"])
 def hello_world(request):
     return Response({"message": "Hello, world!"})
+
+
+@api_view(["GET"])
+def test_api(request):
+    data = {"message": "Hello from Django!"}
+    return Response(data)
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import DjangoUrl from './constants';
 import './App.css'
 
 const App = () => {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch(`${DjangoUrl}/api/test`)
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+      .catch((error) => console.error('Error Fetching Data:', error));
+  }, [])
+
   return (
-    <div className="flex justify-center items-center h-screen font-extrabold">
-      App
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-4xl font-bold">{message}</h1>
     </div>
   )
 }
